076ec92b6d7106ec7bbe072941eb2d340efc8a37
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2026-01-30 11:35:42 -0300

    versao para envio

--- ms-transacoes/src/main/java/br/com/beca/ms_transacoes/infra/feign/CambioClient.java
+++ ms-transacoes/src/main/java/br/com/beca/ms_transacoes/infra/feign/CambioClient.java
@@ -2,6 +2,7 @@ package br.com.beca.ms_transacoes.infra.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "brasilapi-cambio",
--- ms-transacoes/src/main/java/br/com/beca/ms_transacoes/infra/feign/CambioDTO.java
+++ ms-transacoes/src/main/java/br/com/beca/ms_transacoes/infra/feign/CambioDTO.java
@@ -2,6 +2,8 @@ package br.com.beca.ms_transacoes.infra.feign;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import java.time.LocalDateTime;
+
 public record CambioDTO(
         String currency,
         String name,
